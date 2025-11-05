01fc7db3fbe4ccf8abf4edab0f2b71190ae8a9b0
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-11-04 19:16:40 -0800

    7:16 PM

--- Lab 29.cpp
+++ Lab 29.cpp
@@ -2,8 +2,25 @@
 // IDE used: VS Code
 
 #include <iostream>
+#include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+
 using namespace std;
+
+void printPlayers(map<string,array<list<Item>,3>>& Player);
+
+const int maxRarity=10,lowRarity=1;
+const int maxDurability=10,lowDurability=0; // If 
+struct Item{
+    string itemname;
+    int durability;
+    int Rarity;
+};
+
 int main(){
+    map<string,array<list<Item>,3>> Player;
 
     return 0;
 }
