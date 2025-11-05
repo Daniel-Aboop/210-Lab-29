04a1e34d4d9fdf646dc3d39e0be42a3688e6ac02
Daniel-Aboop <danielsantisteban286@gmail.com>
2025-11-04 19:26:56 -0800

    7:26 PM

--- Lab 29.cpp
+++ Lab 29.cpp
@@ -6,21 +6,41 @@
 #include <map>
 #include <list>
 #include <algorithm>
+#include <random>
 
 using namespace std;
 
-void printPlayers(map<string,array<list<Item>,3>>& Player);
 
-const int maxRarity=10,lowRarity=1;
-const int maxDurability=10,lowDurability=0; // If 
+
+const int maxRarity=10,lowRarity=1; //If other item has higher rarity take swap items.
+const int maxDurability=10,lowDurability=0; // If durability reaches 0 Item breaks
 struct Item{
     string itemname;
     int durability;
     int Rarity;
+
+    void checkdurability(){
+        if(durability<=lowDurability){
+            itemname="";
+        }
+    }
 };
+
+void printPlayers(map<string,array<list<Item>,3>>& Player);
+void trading(map<string,array<list<Item>,3>>& Player);
+void lootingItems(map<string,array<list<Item>,3>>& Player);
+void itemDamage(map<string,array<list<Item>,3>>& Player);
+void repairitem(map<string,array<list<Item>,3>>& Player);
+void robbed(map<string,array<list<Item>,3>>& Player);
+void replaceitems(map<string,array<list<Item>,3>>& Player);
 
 int main(){
     map<string,array<list<Item>,3>> Player;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> ranNum(1, 100);// Number for % chances
+    uniform_int_distribution<int> ranPartyMem(1, 8);// Random Party member chosen for event.
+    
 
     return 0;
 }
